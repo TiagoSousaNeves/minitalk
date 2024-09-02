459903fa79ae22d2fe68109308c5d116cc0cfc0e
tcosta-f <tiagosousaneves@gmail.com>
2024-09-02 01:48:41 +0100

    Update minitalk_bonus.h

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -6,14 +6,14 @@
 /*   By: tcosta-f <tcosta-f@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 16:22:08 by tcosta-f          #+#    #+#             */
-/*   Updated: 2024/08/26 18:59:38 by tcosta-f         ###   ########.fr       */
+/*   Updated: 2024/09/02 01:37:49 by tcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
-# include "ft_printf/ft_printf.h"
+# include "../ft_printf/ft_printf.h"
 # include <unistd.h>
 # include <signal.h>
 
